584468319817d55c014d07067421d170ae6d6d9a
Reini Urban <rurban@cpanel.net>
2015-03-22 20:44:08 +0100

    warnings: move to ext/warnings as shared lib
    use gperf, with post-processing as in parrot
    but remove the #line directives. they are just wrong.
    
    add rules to the main Makefile, not a dyn_ext, but a boot_ext.
    add CCCDLFLAGS to Makefile needed to compile a shared lib
    
    regen_lib.pl: defer load of Text::Wrap to run-time.
    Note: xsubpp cannot deal with paths. need to chdir into it
    
    Perl_warnings_lookup now calls the new perfect hash lookup
    and falls back to the dynamic %warnings::_Bits lookup
    which consists of SVt_PVIV with offset as IV
    and bits + deadbits as PV, allocated to max 32 byte (255 bits).
    register_categories adds to %warnings::_Bits.
    
    By allocating the max size we don't need to extend the values
    when a new category is added.
    dynamic var $LAST_BIT holds the global (const size + dynamic size) *2,
    as before.
    Most other previous globals are discontinued.
    
    add a static ws struct for an intermediate lookup result from the
    dynamic hash.
    add a writable struct Perl_warnings_dyn to be able to change the "all" key.
    
    use a special newWSV() to zero all bits.
    fix the no warnings; case pushing "all" as arg
    add -Dv to import and unimport
    copy over the string with the following lengths:
    pvs for static compile-time length
    pvh for static length from header constants (just WARNsize, the rest is zeroed)
    pv for the full dynamic hash lengths (WARN_MAX_BYTES)
    
    in XS caller(0) is PL_curcop, because XS does not track caller.
    
    we error now on the new limitation: the size is hardcoded to max 255 warnings.
    we could fix this when needed by growing all %Bits entries.
    
    xs does not save a callstack, so the short_error_loc is always wrong.
    so disable the XS calls to the callstack using warnings functions, add new PP versions.
    rewrite Carp Perl_short_error_loc to use the PP variant Carp::short_error_loc.
    
    TODO: USE_PP_CARP all over, no Carp as xs
    
    fix some clients:
    deprecate needs to use warnings::warnif, not the internal %Offset.
    
    add keys and exist API:
    
    several CPAN modules need the keys and exists on %Offsets, just to get
    the list and to see if it exists. Export that.
    
    A full backwards compat API, with tie magic to Bits, Offset and DeadBits not yet
    
    to be rewritten:
      warnings::everywhere, warnings::pedantic
      warnings::DynamicScope
      Test::Warn
      qbit
    
    simple fixes:
      strictures, Object::Previous, Scalar::Random::PP::OO
      Scope::Upper, Mousse, Test::Vars, sanity
    
    disable one more feature warnings:
    
    disable experimental::const_attr as default also.
    refactor to list to turn warn lexicals warnings off.
    
    PL_curcop/PL_compiling mixup:
    
    see mg.c: only set the global PL_compiling, not any lexical curcop.
    At runtime, it's not safe to manipulate PL_curcop: it may be
    an op shared between threads. Use a non-shared COP.
    
    fixes at least the failing op test, but not 9enabled yet.
    
    also optimize on DEBUGGING builds
    
    fix more 9enabled test
    
    provide Usage arity error for warn/warnif with 0 args.
    remaining fails: 24, 30, 32, 34, 36, 40, 46, 51-52, 54-56
    
    fix stack underflow:
    
    cperl -Dlotsv -Mwarnings=misc -e0
    when importing one argument, in the call to _bits
    
    STACK 0: MAIN
    STACK 1: REQUIRE
      CX 0: EVAL   =>  [STACK UNDERFLOW!!!]
    
    make _do_bits a static function, and do not call it via
    call_pv. adjust the FATAL logic.
    untested with carp-xs, for ax.

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1580,9 +1580,8 @@ use File::Glob qw(:case);
 
     'warnings' => {
         'FILES'      => q[
-                 lib/warnings
-                 lib/warnings.{pm,t}
-                 regen/warnings.pl
+                 ext/warnings.{pm,t}
+                 ext/warnings_c.PL
                  t/lib/warnings
         ],
     },
