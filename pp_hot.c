87d7dca03925400ffb8b39bac4e2d68a17739e77
Reini Urban <rurban@cpanel.net>
2016-06-01 18:52:48 +0200

    exact_arith: improve implementation
    no temp. sp, only PL_stack_sp

--- pp_hot.c
+++ pp_hot.c
@@ -792,8 +792,9 @@ PPt(pp_add, "(:Number,:Number):Number")
                 if (BUILTIN_UADD_OVERFLOW(auv, buv, &result)) {
 #ifdef PERL_EXACT_ARITH
                     if (UNLIKELY(IS_EXACT_ARITH)) {
+                        PUTBACK;
                         bigint_arith("badd", svl, svr);
-                        RETURN;
+                        return NORMAL;
                     }
                     else
 #endif
@@ -811,8 +812,9 @@ PPt(pp_add, "(:Number,:Number):Number")
                 if (BUILTIN_SADD_OVERFLOW(aiv, biv, &value)) {
 #ifdef PERL_EXACT_ARITH
                     if (UNLIKELY(IS_EXACT_ARITH)) {
+                        PUTBACK;
                         bigint_arith("badd", svl, svr);
-                        RETURN;
+                        return NORMAL;
                     }
                     else
 #endif
@@ -881,8 +883,9 @@ PPt(pp_add, "(:Number,:Number):Number")
 		    else {
 #ifdef PERL_EXACT_ARITH
                         if (UNLIKELY(IS_EXACT_ARITH)) {
+                            PUTBACK;
                             bigint_arith("badd", svl, svr);
-                            RETURN;
+                            return NORMAL;
                         }
                     else
 #endif
@@ -894,8 +897,9 @@ PPt(pp_add, "(:Number,:Number):Number")
 	    } /* Overflow, drop through to NVs.  */
 #ifdef PERL_EXACT_ARITH
             else if (UNLIKELY(IS_EXACT_ARITH)) {
+                PUTBACK;
                 bigint_arith("badd", svl, svr);
-                RETURN;
+                return NORMAL;
             }
 #endif
 #endif
