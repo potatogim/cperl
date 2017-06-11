a6de3d98e83b350cbe3701efbea8f2938ec7ccd2
Reini Urban <rurban@cpan.org>
2017-06-11 09:49:44 +0200

    ffi: add 0.01c helper module
    not mandatory

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1764,6 +1764,7 @@ use File::Glob qw(:case);
                 lib/feature.{pm,t}
                 lib/feature/
                 lib/filetest.{pm,t}
+                lib/ffi.pm
                 lib/h2ph.t
                 lib/h2xs.t
                 lib/integer.{pm,t}
